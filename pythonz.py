4a6aa24582f17cee7e6c34709ea2518badc22e59
ziyaurrahaman <31872952+ziyaurrahaman@users.noreply.github.com>
2017-09-12 08:32:53 +0530

    Add files via upload

---
+++ pythonz.py
@@ -0,0 +1,10 @@
+Python 3.4.3 (v3.4.3:9b73f1c3e601, Feb 24 2015, 22:43:06) [MSC v.1600 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> ================================ RESTART ================================
+>>> 
+swapping code
+enter the value of x:5
+enter the value of y:6
+the value of x after swapping: 6
+the value of y after swapping: 5
+>>> 
